5c3b00c15cb47599afa8255dc741f8b873ab482b
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-20 14:49:59 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 from keirin_json import get_race_data
 
+DAY_CODES = {"1"}
+NIGHT_CODES = {"3"}
+
 data = get_race_data()
 
 for race in data["RaceList"]:
 
+    code = race["kubunIconName"]
+
+    if code in DAY_CODES:
+        session = "day"
+
+    elif code in NIGHT_CODES:
+        session = "night"
+
+    else:
+        continue
+
     print("場:", race["keirinjoName"])
     print("グレード:", race["gradeIconName"])
     print("何日目:", race["nichijiIconName"])
-    print("区分:", race["kubunIconName"])
+    print("区分:", session)
     print("------------------")
