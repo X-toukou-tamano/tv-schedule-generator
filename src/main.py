6ff05d303eb99fb464247710d53c2fd6c3f78bab
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-20 14:47:12 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,4 +2,10 @@ from keirin_json import get_race_data
 
 data = get_race_data()
 
-print(data)
+for race in data["RaceList"]:
+
+    print("場:", race["keirinjoName"])
+    print("グレード:", race["gradeIconName"])
+    print("何日目:", race["nichijiIconName"])
+    print("区分:", race["kubunIconName"])
+    print("------------------")
