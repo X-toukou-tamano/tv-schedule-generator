f99396635f9eb358e9debbf2b2f2e44de0ed728f
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-20 14:20:33 +0900

    Update keirin_json.py

--- src/keirin_json.py
+++ src/keirin_json.py
@@ -1,7 +1,10 @@
 import requests
+
 DAY_CODES = {"1"}
 NIGHT_CODES = {"3"}
+
 def get_race_data():
+
     url = (
         "https://keirin.jp/pc/json"
         "?kaisaibikbn=0"
@@ -10,9 +13,11 @@ def get_race_data():
         "&dispid=PJ0315"
         "&type=JSJ048"
     )
-     data = requests.get(
+
+    data = requests.get(
         url,
         headers={"User-Agent": "Mozilla/5.0"},
         timeout=30
     ).json()
+
     return data
