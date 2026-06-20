60cdafaba71c95413fb01e1a26619d10b0499cff
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-20 13:18:37 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Hello KEIRIN")
+import requests
+
+url = "https://keirin.jp/pc/top"
+
+response = requests.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
+    timeout=30
+)
+
+print("status:", response.status_code)
+print(response.text[:1000])
