26835e176d862b9308c32dc3de87166e5e813a22
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-21 09:11:33 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,30 +1,5 @@
-from keirin_json import get_race_data
+from excel_reader import get_today_tracks
 
-DAY_CODES = {"1"}
-NIGHT_CODES = {"3"}
+tracks = get_today_tracks()
 
-data = get_race_data()
-
-for race in data["RaceList"]:
-
-    code = race["kubunIconName"]
-
-    if code in DAY_CODES:
-        session = "day"
-
-    elif code in NIGHT_CODES:
-        session = "night"
-
-    else:
-        continue
-
-    item = {
-        "name": race["keirinjoName"],
-        "grade": race["gradeIconName"],
-        "status": race["nichijiIconName"],
-        "session": session
-    }
-
-    print(item)
-
-    
+print(tracks)
