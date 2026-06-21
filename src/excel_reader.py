ae57f7d899013abd7ef2a010775dfc2b9b591bf7
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-21 09:17:18 +0900

    Update excel_reader.py

--- src/excel_reader.py
+++ src/excel_reader.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from openpyxl import load_workbook
 
 def get_today_tracks():
 
     today = datetime.today()
 
-    print(today)
+    print("月:", today.month)
+    print("日:", today.day)
 
     return []
