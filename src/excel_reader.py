cea533e1c9a8da3a36f58d16504426838de2cc4b
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-21 11:19:11 +0900

    Update excel_reader.py

--- src/excel_reader.py
+++ src/excel_reader.py
@@ -8,8 +8,12 @@ def get_today_tracks():
     print("月:", today.month)
     print("日:", today.day)
 
-    wb = load_workbook("R８上期確定版(サテライト込み)_2026.02.14.xlsx")
+    wb = load_workbook(
+        "R８上期確定版(サテライト込み)_2026.02.14.xlsx",
+        data_only=True
+    )
 
-    print(wb.sheetnames)
+    for sheet_name in wb.sheetnames:
+        print(sheet_name)
 
     return []
