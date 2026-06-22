43079db89751ef964de906f896df797e3748a1b5
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-22 15:06:31 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from excel_reader import get_today_tracks
 
 tracks = get_today_tracks()
+def get_today_tracks():
+    return [
+        "玉野",
+        "岸和田"
+    ]
 
 print("開催場数:", len(tracks))
 print(tracks)
