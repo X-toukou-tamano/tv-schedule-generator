df59f9b15e62f935cd56da612d0340bf4b3ddcb3
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-22 15:13:59 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def index():
     return "TV用パワポ自動化システム"
 
+@app.route("/upload")
+def upload():
+    return """
+    <h1>Excelアップロード</h1>
+
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="excel">
+        <button type="submit">
+            アップロード
+        </button>
+    </form>
+    """
+
 if __name__ == "__main__":
     app.run(debug=True)
