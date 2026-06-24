2ad013720e1ab816e3b8f6e80de6b300748a1baf
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-24 16:33:43 +0900

    Update generate_today_ppt.py

--- src/generate_today_ppt.py
+++ src/generate_today_ppt.py
@@ -1,1 +1,25 @@
+from app import get_today_sorted_data
+from ppt_generator import create_powerpoint
 
+
+def main():
+    (
+        (
+            day_events,
+            night_events,
+            _,
+            _,
+        ),
+        _,
+    ) = get_today_sorted_data()
+
+    create_powerpoint(
+        day_events,
+        night_events
+    )
+
+    print("PPT生成完了")
+
+
+if __name__ == "__main__":
+    main()
