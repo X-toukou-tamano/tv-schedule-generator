7887a3750ec9a91a07d61aebff5187a5b0bcf4fc
X-toukou-tamano <yasuhiro.nishizaki@chariloto-ps.com>
2026-06-25 12:55:57 +0900

    Update generate_today_ppt.py

--- src/generate_today_ppt.py
+++ src/generate_today_ppt.py
@@ -1,8 +1,58 @@
+import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from database import (
+    create_tables,
+    get_summary,
+    save_records,
+)
+
+from excel_reader import parse_excel
 from today_service import get_today_sorted_data
 from ppt_generator import create_powerpoint
 
-
 def main():
+
+    create_tables()
+
+    summary = get_summary()
+
+    if summary[2] == 0:
+
+        today = datetime.now(
+            ZoneInfo("Asia/Tokyo")
+        ).date()
+
+        if today.month >= 4:
+            reiwa = today.year - 2018
+        else:
+            reiwa = today.year - 2019
+
+        year = f"R{reiwa}"
+
+        if 4 <= today.month <= 9:
+            term = "上期"
+        else:
+            term = "下期"
+
+        target_path = os.path.join(
+            "uploads",
+            f"{year}_{term}.xlsx"
+        )
+
+        if os.path.exists(
+            target_path
+        ):
+
+            records = parse_excel(
+                target_path
+            )
+
+            save_records(
+                records
+            )
+
     (
         (
             day_events,
@@ -19,7 +69,3 @@ def main():
     )
 
     print("PPT生成完了")
-
-
-if __name__ == "__main__":
-    main()
